1d6a7602158bd3d2836aa7385a750f8ff2e79eed
alejandrogomez <alegomez1@me.com>
2019-11-02 18:32:27 -0400

    new files

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.scss'
-import { BrowserRouter as Router } from 'react-router-dom'
-import App from './components/App.jsx'
-import 'bootstrap/dist/css/bootstrap.css'
-import axios from 'axios'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-axios.defaults.withCredentials = false
-// import registerServiceWorker from './registerServiceWorker';
+ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
-)
-// registerServiceWorker();
+serviceWorker.unregister();
+
+export default App;
---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Put all API endpoints under '/api'
+app.get('/apps', (req, res) => {
+    res.send('HELLO')
+});
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.asdfasdfasdfasf
+app.get('*', (req, res) => {
+    console.log('the dirname', __dirname)
+    res.sendFile('/client/build/index.html ' ,  { root : __dirname })})
+  
+  const port = process.env.PORT || 5000;
+  app.listen(port);
+  
+  console.log(`Listening on ${port}`);
