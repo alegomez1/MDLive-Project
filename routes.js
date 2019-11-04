b4142791a91eb05d07c85ae88b3eefc0072c9b51
alejandrogomez <alegomez1@me.com>
2019-11-04 15:16:12 -0500

    fixed problem with max logic

--- routes.js
+++ routes.js
@@ -10,7 +10,6 @@ let list = require('./list.json')
  * Depending on what case runs, it also checks to see what the order is wether it's ascending(asc) or descending(desc), and will res.json the proper array
  */
 router.get('/', (req, res, next) => {
-
   let rangeBy = req.query.rangeBy
   let start = req.query.start
   let end = req.query.end
@@ -19,85 +18,90 @@ router.get('/', (req, res, next) => {
   let appArray = []
 
   //Makes sure the rangeBy query has a value in it
-  if(rangeBy != ''){
-  switch (rangeBy) {
-    //If rangeBy = id
-    case 'id':
-      /**
-       * This for-loop goes through the list.json file based on the start and end query values
-       * If i is less than the max query value indicated, it adds it to the appArray variable, other wise it just iterates i
-       * If there is no max query value, it adds the items from the start to end values of the list.json
-       */
-      for (let i = start - 1; i < end; i++) {
-        if(max!=undefined){
-          if(i<max){
-            appArray.push(list[i])
-          } else{
-            i++
-          }
-        }else{
-          appArray.push(list[i])
-        }
-      }
-      //Depending on the order query value, it will res.json the appArray regularly, or reversed
-      if(order === 'asc'){
-      res.json(appArray)
-    }else if(order === 'desc'){
-      res.json(appArray.reverse())
-    }else{
-      res.json(appArray)
-    }
-      break
-    //If rangeBy = name
-    case 'name':
-      let start2 = 0
-      let end2 = 0
-      /**
-       * This for-loop finds the IDs of the start and end app names
-       * These IDs are then assigned to the start2 and end2 variables whcih are used in the next loop
-       */
-      for (let i = 0; i < list.length; i++) {
-        if (list[i].name === start) {
-          start2 = list[i].id
-        } else if (list[i].name === end) {
-          end2 = list[i].id
-        }
-      }
-      //This if statement checks to make sure these variables have an actual value
-      if (start2 != 0 && end2 != 0) {
-      /**
-       * This for-loop goes through the list.json file based on the start and end query values
-       * If i is less than the max query value indicated, it adds it to the appArray variable, other wise it just iterates i
-       * If there is no max query value, it adds the items from the start to end values of the list.json
-       */
-        for (let i = start2 - 1; i < end2; i++) {
-          if(max!=undefined){
-            if(i<=max){
+  if (rangeBy != '') {
+    switch (rangeBy) {
+      //If rangeBy = id
+      case 'id':
+        max = Number(max) + (Number(start) - 1) //This accounts for when the value of max is less than the value of start
+
+        /**
+         * This for-loop goes through the list.json file based on the start and end query values
+         * If i is less than the max query value indicated, it adds it to the appArray variable, other wise it just iterates i
+         * If there is no max query value, it adds the items from the start to end values of the list.json
+         */
+
+        for (let i = start - 1; i < end; i++) {
+          if (max != undefined) {
+            if (i < max) {
               appArray.push(list[i])
-            } else{
+            } else {
               i++
             }
-          }else{
+          } else {
             appArray.push(list[i])
           }
         }
-      //Depending on the order query value, it will res.json the appArray regularly, or reversed
-        if(order === 'asc'){
+        //Depending on the order query value, it will res.json the appArray regularly, or reversed
+        if (order === 'asc') {
           res.json(appArray)
-        }else if(order === 'desc'){
+        } else if (order === 'desc') {
           res.json(appArray.reverse())
-        }else{
+        } else {
           res.json(appArray)
         }
-      }
-      break;
+        break
+      //If rangeBy = name
+      case 'name':
+        max = Number(max) + (Number(start) - 1) //This accounts for when the value of max is less than the value of start
+
+        let start2 = 0
+        let end2 = 0
+        /**
+         * This for-loop finds the IDs of the start and end app names
+         * These IDs are then assigned to the start2 and end2 variables whcih are used in the next loop
+         */
+        for (let i = 0; i < list.length; i++) {
+          if (list[i].name === start) {
+            start2 = list[i].id
+          } else if (list[i].name === end) {
+            end2 = list[i].id
+          }
+        }
+        //This if statement checks to make sure these variables have an actual value
+        if (start2 != 0 && end2 != 0) {
+          /**
+           * This for-loop goes through the list.json file based on the start and end query values
+           * If i is less than the max query value indicated, it adds it to the appArray variable, other wise it just iterates i
+           * If there is no max query value, it adds the items from the start to end values of the list.json
+           */
+          for (let i = start2 - 1; i < end2; i++) {
+            if (max != undefined) {
+              if (i <= max) {
+                appArray.push(list[i])
+              } else {
+                i++
+              }
+            } else {
+              appArray.push(list[i])
+            }
+          }
+          //Depending on the order query value, it will res.json the appArray regularly, or reversed
+          if (order === 'asc') {
+            res.json(appArray)
+          } else if (order === 'desc') {
+            res.json(appArray.reverse())
+          } else {
+            res.json(appArray)
+          }
+        }
+        break
       //Default case is to res.json the entire list of items
       default:
-          res.json(list)
-  }
-}else{
+        res.json(list)
+    }
+  } else {
     res.json(list)
-}
+  }
 })
 
 module.exports = router
